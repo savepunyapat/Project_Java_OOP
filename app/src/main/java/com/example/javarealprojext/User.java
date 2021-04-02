660c9c6594790ede9135a6f7d7ec0de355cc870d
Punyapat <punyapat1412@gmail.com>
2021-04-02 13:03:54 +0700

    First Project!!!!!!

---
+++ app/src/main/java/com/example/javarealprojext/User.java
@@ -0,0 +1,11 @@
+package com.example.javarealprojext;
+
+public class User {
+    private String name;
+    private String id;
+    public User(){}
+    public User(String n,String nid){
+        this.id = nid;
+        this.name = n;
+    }
+}
